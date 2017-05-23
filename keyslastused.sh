817bc1368e760e7c6b039123dbd455739b470166
Tree <trenigma@users.noreply.github.com>
2017-05-23 13:01:11 -0400

    Update keyslastused.sh
    changed case on 'lasttimeused' to 'LastTimeUsed' edited comment to be more accurate

--- keyslastused.sh
+++ keyslastused.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##pulls 'LastTimeUsed' for AWS IAM users' access keys. writes them to a .csv file 
+##pulls 'LastTimeUsed' for AWS IAM users' access keys
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo -n "$line "
     aws iam get-access-key-last-used --access-key $line --output text | cut -f 2 >> keylast.csv
@@ -8,5 +8,5 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 done < "keys_hlprod.txt"
 
 printf '\n'
-echo "these are all the lasttimeused for each user key =)"
+echo "these are all the LastTimeUsed for each user key =)"
 printf '\n'
