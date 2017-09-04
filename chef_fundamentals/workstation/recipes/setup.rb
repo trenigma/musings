fe4f9b36691fc52228ca34661cd40c15d95f5f77
trenigma <derek.ogletree@gmail.com>
2017-09-04 20:52:26 +0000

    initial workstation cookbook commit

---
+++ chef_fundamentals/workstation/recipes/setup.rb
@@ -0,0 +1,16 @@
+package 'tree' do
+	action :install
+end
+
+package 'git' do
+  action :install
+end
+
+package 'ntp'
+
+file '/etc/motd' do
+	content 'This server is the property of LilTree'
+	action :create
+	owner 'root'
+	group 'root'
+end
